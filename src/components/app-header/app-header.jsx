dcd1f58a451e7c42ceea08846b492d48a477ce57
neekit95 <nikita-ribalkin@mail.ru>
2023-08-23 20:42:00 +0300

    header ended

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -2,26 +2,39 @@ import React from "react";
 import appHeaderStyle from "./app-header-style.module.scss";
 import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
 import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-
+import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 function AppHeader() {
   return (
     <>
       <header className={appHeaderStyle.header}>
         <div className={appHeaderStyle.wrapper}>
           <section className={appHeaderStyle.navbar}>
-            <a className={appHeaderStyle.a} href="/">
-            <nav className={appHeaderStyle.nav}>
-              <BurgerIcon type="primary" />
-              <p className="text text_type_main-default">Конструктор</p>
-            </nav>
+            <div className={appHeaderStyle.div}>
+              <a className={appHeaderStyle.a} href="/">
+                <nav className={appHeaderStyle.nav}>
+                  <BurgerIcon type="primary" />
+                  <p className={appHeaderStyle.p}>Конструктор</p>
+                </nav>
+              </a>
+              <a className={appHeaderStyle.a} href="/">
+                <nav className={appHeaderStyle.nav}>
+                  <ListIcon type="primary" />
+                  <p className="text text_type_main-default">Лента заказов</p>
+                </nav>
+              </a>
+            </div>
+            <a className={appHeaderStyle.a_logo} href="/">
+              
+                <Logo />
+              
             </a>
-            
-            <nav className={appHeaderStyle.nav}></nav>
-            <nav className={appHeaderStyle.nav}>
-              <Logo />
-            </nav>
-            <nav className={appHeaderStyle.nav}></nav>
-            <section className={appHeaderStyle.section}> </section>
+            <a className={appHeaderStyle.a} href="/">
+              <nav className={appHeaderStyle.nav}>
+                <ProfileIcon type="primary" />
+                <p className="text text_type_main-default">Личный кабинет</p>
+              </nav>
+            </a>
           </section>
         </div>
       </header>
