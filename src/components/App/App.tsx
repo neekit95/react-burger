59fa618223d66c6f9276d2af57df9af265cc16d4
neekit95 <nikita-ribalkin@mail.ru>
2023-08-24 14:13:13 +0300

    шрифты

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,16 +7,15 @@ import BurgerConstruction from '../burgrer-counstruction/burgrer-counstruction';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <div className={appStyle.wrapper}>
         <AppHeader />
         <div className={appStyle.main}>
-        <h2 className={appStyle.h2}>Соберите бургер</h2>
           <BurgrerIngridients />
           <BurgerConstruction />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/burger-ingridients/burgrer-ingridients.jsx
+++ src/components/burger-ingridients/burgrer-ingridients.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import burgerIngridientsStyle from "./burger-ingridients.module.scss";
 function BurgrerIngridients() {
   return (
     <>
-      
+      <main>
+        <h1 className={burgerIngridientsStyle.h1}>Соберите бургер</h1>
+      </main>
     </>
-  )
+  );
 }
 
 export default BurgrerIngridients;
