68cccc6c105b55f56d49ea01054218fc57492c4b
neekit95 <nikita-ribalkin@mail.ru>
2023-08-24 15:16:13 +0300

    tab problem

--- src/components/burger-ingridients/burgrer-ingridients.jsx
+++ src/components/burger-ingridients/burgrer-ingridients.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import burgerIngridientsStyle from "./burger-ingridients.module.scss";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+// import { useState } from "react";
+
 function BurgrerIngridients() {
+  const [current, setCurrent] = React.useState("one");
+  const handleTabClick = (value) => {
+    setCurrent(value);
+  };
   return (
-    <>
-      <main>
-        <h1 className={burgerIngridientsStyle.h1}>Соберите бургер</h1>
-      </main>
-    </>
+    <main>
+      <h1 className={burgerIngridientsStyle.h1}>Соберите бургер</h1>
+
+      <div style={{ display: "flex" }}>
+        <Tab
+          value="one"
+          active={current === "one"}
+          onClick={() => handleTabClick("one")}
+        >
+          One
+        </Tab>
+        <Tab
+          value="two"
+          active={current === "two"}
+          onClick={() => handleTabClick("two")}
+        >
+          Two
+        </Tab>
+        <Tab
+          value="three"
+          active={current === "three"}
+          onClick={() => handleTabClick("three")}
+        >
+          Three
+        </Tab>
+      </div>
+    </main>
   );
 }
 
