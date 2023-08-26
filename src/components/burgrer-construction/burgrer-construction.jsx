c9d9ca0c46b6d382bc2ff3f77325d13850e7e9ef
neekit95 <nikita-ribalkin@mail.ru>
2023-08-26 15:05:00 +0300

    card padding

--- src/components/burgrer-construction/burgrer-construction.jsx
+++ src/components/burgrer-construction/burgrer-construction.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import burgerConstructionStyle from "./burgrer-construction.module.scss";
 import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function BurgerConstruction(props) {
   const burgerArr = props.burgerArr;
   console.log(burgerArr);
- 
 
   const displayBurgerArr = burgerArr.map((item) => (
     <ConstructorElement
-      key={item.id} // Добавьте уникальный ключ
-      type="top"
-      isLocked={true}
+      style ={{}} //добавить картинку
+      key={item.id} 
+      type="default"
+      isLocked={false}
       text={item.name}
       price={item.price}
       thumbnail={item.image_mobile}
@@ -20,10 +21,32 @@ function BurgerConstruction(props) {
 
   return (
     <main className={burgerConstructionStyle.main}>
-      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-    
-        {displayBurgerArr}
-      </div>
+      <section className={burgerConstructionStyle.scroll}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail={props.image_mobile}
+          />
+
+          {displayBurgerArr}
+
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={200}
+            thumbnail={props.image_mobile}
+          />
+        </div>
+      </section>
+
+      <section className="burgerConstructionStyle.price">
+        <p className="text text_type_digits-medium">123</p>
+        <CurrencyIcon type="primary" />
+      </section>
     </main>
   );
 }
